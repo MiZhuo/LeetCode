af2b218d7c6c888a520fb18f34f2ada58d58b40f
wulibin <wulibin0809@gmail.com>
2019-05-07 23:13:13 +0800

    reverse

---
+++ Java/src/main/java/LeetCode20190507/AddTwoNumbers.java
@@ -0,0 +1,37 @@
+package src.main.java.LeetCode20190507;
+
+public class AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        ListNode p = l1,q = l2,cur = listNode;
+        int car = 0;
+        while (p != null || q != null){
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = car + x + y;
+            car = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if(p != null){
+                p = p.next;
+            }
+            if(q != null){
+                q = q.next;
+            }
+        }
+        if(car > 0){
+            cur.next = new ListNode(car);
+        }
+
+        return listNode.next;
+    }
+}
