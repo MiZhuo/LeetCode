c8e43b6334ec1519643a49be67d549a2b0b4245f
mizhuo <libin.wu@sunyard.com>
2019-05-29 13:37:40 +0800

    reorderLogFiles

--- Java/src/main/java/LeetCode20190528/ReorderLogFiles.java
+++ Java/src/main/java/LeetCode20190528/ReorderLogFiles.java
@@ -29,10 +29,9 @@ public class ReorderLogFiles {
     public String[] reorderLogFiles(String[] logs) {
         List<String> letters = new ArrayList<>();
         List<String> digits = new ArrayList<>();
-        boolean isNum = true;
         for (String log : logs) {
-            isNum = true;
-            for (int i = 1; i < log.length(); i++) {
+            boolean isNum = true;
+            for (int i = log.indexOf(' '); i < log.length(); i++) {
                 if(log.charAt(i) == ' '){
                     continue;
                 }
@@ -47,8 +46,36 @@ public class ReorderLogFiles {
                 digits.add(log);
             }
         }
-        Arrays.sort(letters.toArray());
+        int size = letters.size();
         letters.addAll(digits);
-        return (String[]) letters.toArray();
+        String[] arr = new String[letters.size()];
+        letters.toArray(arr);
+        return LogSort(arr,size);
+    }
+
+    /**
+     * 对字母日志进行排序
+     * @param arr
+     * @param size
+     * @return
+     */
+    private String[] LogSort(String[] arr,int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for(int j = arr[i].indexOf(' ') + 1, k = arr[i + 1].indexOf(' ') + 1; j < arr[i].length() && k < arr[i + 1].length();j++,k++){
+                if(arr[i].charAt(j) > arr[i + 1].charAt(k)){
+                    String temp = "";
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    break;
+                }else if (arr[i].charAt(j) == arr[i + 1].charAt(k)){
+                    continue;
+                }else{
+                    break;
+                }
+            }
+
+        }
+        return arr;
     }
 }
