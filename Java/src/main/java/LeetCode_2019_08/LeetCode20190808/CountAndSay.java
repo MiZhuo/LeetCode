f686bd692a4a9bc7f2ed588fa69148ed44d3848c
mizhuo <libin.wu@sunyard.com>
2019-08-09 17:47:07 +0800

    countAndSay

---
+++ Java/src/main/java/LeetCode_2019_08/LeetCode20190808/CountAndSay.java
@@ -0,0 +1,56 @@
+package LeetCode_2019_08.LeetCode20190808;
+
+/**
+ * Create by MiZhuo on 2019/8/8
+ * 报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+ *
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 1 被读作  "one 1"  ("一个一") , 即 11。
+ * 11 被读作 "two 1s" ("两个一"）, 即 21。
+ * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+ *
+ * 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+ *
+ * 注意：整数顺序将表示为一个字符串。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "1"
+ * 示例 2:
+ *
+ * 输入: 4
+ * 输出: "1211"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-and-say
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CountAndSay {
+    public String countAndSay(int n) {
+        getString(String.valueOf(n));
+        return "";
+    }
+    public String getString(String s){
+        if(s.length() == 1){
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        int loop = 1;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) != s.charAt(i + 1)){
+                sb.append(loop).append(i);
+                loop = 1;
+            }else{
+                loop++;
+            }
+        }
+        return sb.toString();
+    }
+}
